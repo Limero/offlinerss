4d787cbd2f7f5f03b9523a88dbbb9546c4473bbd
David Pedersen <limero@me.com>
2023-06-24 22:06:41 +0200

    Make RemoveDuplicates slice helper generic

---
+++ helpers/slice.go
@@ -0,0 +1,18 @@
+package helpers
+
+func RemoveDuplicates[T comparable](slice []T) []T {
+	/*
+		Remove duplicates in slice
+	*/
+	encountered := make(map[T]bool)
+	result := make([]T, 0, len(slice))
+
+	for _, v := range slice {
+		if !encountered[v] {
+			encountered[v] = true
+			result = append(result, v)
+		}
+	}
+
+	return result
+}
--- helpers/string.go
+++ helpers/string.go
@@ -9,20 +9,3 @@ func CondString(condition bool, primary string, fallback string) string {
 	}
 	return fallback
 }
-
-func RemoveDuplicates(slice []string) []string {
-	/*
-		Remove duplicates in string slice
-	*/
-	encountered := make(map[string]bool)
-	result := []string{}
-
-	for _, v := range slice {
-		if !encountered[v] {
-			encountered[v] = true
-			result = append(result, v)
-		}
-	}
-
-	return result
-}
