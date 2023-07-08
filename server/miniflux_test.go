14f639ee2f7cfc3642471961695b86034ef34b96
David Pedersen <limero@me.com>
2023-07-08 13:56:43 +0200

    Add more Miniflux SyncToServer tests

--- server/miniflux_test.go
+++ server/miniflux_test.go
@@ -19,16 +19,42 @@ func TestMinifluxSyncToServer(t *testing.T) {
 		client: mockClient,
 	}
 
+	// Read
 	mockClient.On("UpdateEntries", []int64{1, 2}, miniflux.EntryStatusRead).
 		Return(nil)
+
+	// Unread
 	mockClient.On("UpdateEntries", []int64{3, 4}, miniflux.EntryStatusUnread).
 		Return(nil)
+
+	// Starred
+	mockClient.On("Entry", int64(1)).
+		Return(&miniflux.Entry{Starred: false}, nil)
+	mockClient.On("ToggleBookmark", int64(1)).
+		Return(nil)
+	mockClient.On("Entry", int64(2)).
+		Return(&miniflux.Entry{Starred: true}, nil)
+
+	// Unstarred
+	mockClient.On("Entry", int64(3)).
+		Return(&miniflux.Entry{Starred: true}, nil)
+	mockClient.On("ToggleBookmark", int64(3)).
+		Return(nil)
+	mockClient.On("Entry", int64(4)).
+		Return(&miniflux.Entry{Starred: false}, nil)
 
 	syncToActions := models.SyncToActions{
 		{Id: "1", Action: models.ActionStoryRead},
 		{Id: "2", Action: models.ActionStoryRead},
+
 		{Id: "3", Action: models.ActionStoryUnread},
 		{Id: "4", Action: models.ActionStoryUnread},
+
+		{Id: "1", Action: models.ActionStoryStarred},
+		{Id: "2", Action: models.ActionStoryStarred}, // already starred, should be skipped
+
+		{Id: "3", Action: models.ActionStoryUnstarred},
+		{Id: "4", Action: models.ActionStoryUnstarred}, // already unstarred, should be skipped
 	}
 	err := s.SyncToServer(syncToActions)
 	require.NoError(t, err)
