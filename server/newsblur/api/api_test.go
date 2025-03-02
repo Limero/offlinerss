d7f02dae82598e8cf504c85894d6c72671fc4ef9
David Pedersen <limero@me.com>
2025-03-02 12:33:23 +0700

    Add more newsblur api tests

--- server/newsblur/api/api_test.go
+++ server/newsblur/api/api_test.go
@@ -4,6 +4,7 @@ import (
 	_ "embed"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -12,6 +13,9 @@ import (
 
 //go:embed response/readerfeeds.json
 var respReaderFeeds []byte
+
+func TestLogin(t *testing.T) {
+}
 
 func TestReaderFeeds(t *testing.T) {
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -53,4 +57,101 @@ func TestReaderFeeds(t *testing.T) {
 		{ID: 8312062, Ps: 0, Nt: 0, Ng: 0, FeedAddress: "https://webzine.puffy.cafe/atom.xml", FeedLink: "https://webzine.puffy.cafe/", FeedTitle: "OpenBSD Webzine"},
 		{ID: 7600810, Ps: 0, Nt: 0, Ng: 0, FeedAddress: "https://undeadly.org/cgi?action=rss", FeedLink: "https://undeadly.org/", FeedTitle: "OpenBSD Journal"},
 	}, output.Feeds)
+}
+
+func TestReaderUnreadStoryHashes(t *testing.T) {
+}
+
+func TestReaderStarredStoryHashes(t *testing.T) {
+}
+
+func TestReaderRiverStories_StoryHash(t *testing.T) {
+}
+
+func TestMarkStoryHashesAsRead(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		require.Equal(t, http.MethodPost, r.Method)
+		require.Equal(t, "/reader/mark_story_hashes_as_read", r.RequestURI)
+		require.Equal(t, "application/x-www-form-urlencoded", r.Header.Get("Content-Type"))
+
+		require.NoError(t, r.ParseForm())
+		require.Equal(t, url.Values{"story_hash": []string{"a", "b"}}, r.Form)
+
+		w.WriteHeader(http.StatusOK)
+		_, err := w.Write([]byte("")) // TODO
+		require.NoError(t, err)
+	}))
+	defer ts.Close()
+
+	api := &Newsblur{
+		Hostname: ts.URL,
+		client:   &http.Client{},
+	}
+	require.NoError(t, api.MarkStoryHashesAsRead([]string{"a", "b"}))
+}
+
+func TestMarkStoryHashAsUnread(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		require.Equal(t, http.MethodPost, r.Method)
+		require.Equal(t, "/reader/mark_story_hash_as_unread", r.RequestURI)
+		require.Equal(t, "application/x-www-form-urlencoded", r.Header.Get("Content-Type"))
+
+		require.NoError(t, r.ParseForm())
+		require.Equal(t, url.Values{"story_hash": []string{"a"}}, r.Form)
+
+		w.WriteHeader(http.StatusOK)
+		_, err := w.Write([]byte(""))
+		require.NoError(t, err)
+	}))
+	defer ts.Close()
+
+	api := &Newsblur{
+		Hostname: ts.URL,
+		client:   &http.Client{},
+	}
+	require.NoError(t, api.MarkStoryHashAsUnread("a"))
+}
+
+func TestMarkStoryHashAsStarred(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		require.Equal(t, http.MethodPost, r.Method)
+		require.Equal(t, "/reader/mark_story_hash_as_starred", r.RequestURI)
+		require.Equal(t, "application/x-www-form-urlencoded", r.Header.Get("Content-Type"))
+
+		require.NoError(t, r.ParseForm())
+		require.Equal(t, url.Values{"story_hash": []string{"a"}}, r.Form)
+
+		w.WriteHeader(http.StatusOK)
+		_, err := w.Write([]byte(""))
+		require.NoError(t, err)
+	}))
+	defer ts.Close()
+
+	api := &Newsblur{
+		Hostname: ts.URL,
+		client:   &http.Client{},
+	}
+	require.NoError(t, api.MarkStoryHashAsStarred("a"))
+}
+
+func MarkStoryHashAsUnstarred(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		require.Equal(t, http.MethodPost, r.Method)
+		require.Equal(t, "/reader/mark_story_hash_as_unstarred", r.RequestURI)
+		require.Equal(t, "application/x-www-form-urlencoded", r.Header.Get("Content-Type"))
+
+		require.NoError(t, r.ParseForm())
+		require.Equal(t, url.Values{"story_hash": []string{"a"}}, r.Form)
+
+		w.WriteHeader(http.StatusOK)
+		_, err := w.Write([]byte(""))
+		require.NoError(t, err)
+	}))
+	defer ts.Close()
+
+	api := &Newsblur{
+		Hostname: ts.URL,
+		client:   &http.Client{},
+	}
+	require.NoError(t, api.MarkStoryHashAsUnstarred("a"))
 }
